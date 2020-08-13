d6925d683750c9577055fcd9d153acee156e9957
Thomas Chan <thomas.chan@rga.com>
2020-08-13 10:52:53 -0400

    updated indentation

--- src/util/matrix.py
+++ src/util/matrix.py
@@ -18,7 +18,7 @@ def get_column(A, j):
 
 def make_matrix(num_rows, num_cols, entry_fn):
     return [[entry_fn(i, j)
-             for j in range(num_cols)]
+              for j in range(num_cols)]
             for i in range(num_rows)]
     
 def is_diagonal(i, j):
